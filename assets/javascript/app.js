b858f163bf411013efcc67266c2059cc7516b807
AdamM <ajmrozinski@gmail.com>
2019-07-18 00:22:44 -0400

    buttons now pull 1- gifs related to value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,46 +6,20 @@ var queryItem = "starwars"
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=oeqqCRsnyNGcKSKjAaxiMQ07KuXLGTI1&q=" + queryItem + "&limit=10&offset=0&rating=PG-13&lang=en";
 
 
-function gifGetter(query){
+function gifGetter(query) {
+    $("#gifDump").html("")
     $.ajax({
         url: queryURL,
         method: "get"
-    }).then(function (response){
+    }).then(function (response) {
         console.log(response)
-        for (var i = 0; i < response.data.length; i++){
-           var gifObject = response.data[i]
-           var imgElement = $("<img>")
-           imgElement.attr("src",gifObject.images.downsized_still.url)
-           $("#gifDump").append(imgElement)
-
-
+        for (var i = 0; i < response.data.length; i++) {
+            var gifObject = response.data[i]
+            var imgElement = $("<img>")
+            imgElement.attr("src", gifObject.images.downsized_still.url)
+            $("#gifDump").append(imgElement)
         }
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $.ajax({
-//     url: queryURL,
-//     method: "get"
-// }).then(function (response) {
-//     for (var i = 0; i < response.data.length; i++) {
-//         var gifObject = response.data[i]
-//         var imgElement = $("<img>")
-//         imgElement.attr("src", gifObject.images.downsized.url)
-//         $("#gifDump").append(imgElement)
-//     }
-// })
-
 }
 gifGetter(queryItem)
 
@@ -53,20 +27,26 @@ gifGetter(queryItem)
 for (var i = 0; i < topics.length; i++) {
     var starterButtons = topics[i]
     console.log(topics[i])
-    buttonMaker(starterButtons,i)
+    buttonMaker(starterButtons, i)
 }
 
 
-function buttonMaker(topic,index){
+function buttonMaker(topic, index) {
     var buttonElement = $("<button>")
     buttonElement.addClass("btn bg-info border border-dark mx-1")
     buttonElement.text(topic)
-    buttonElement.attr("value",index)
+    buttonElement.attr("value", index)
     $("#buttonDump").append(buttonElement)
 };
 
-$("#buttonDump").on("click", ".btn", function(e) {
-    console.log(e.target.value)
+$("#buttonDump").on("click", ".btn", function (e) {
+    console.log(e)
+    queryItem = topics[e.target.value]
+    queryURL = "https://api.giphy.com/v1/gifs/search?api_key=oeqqCRsnyNGcKSKjAaxiMQ07KuXLGTI1&q=" + queryItem + "&limit=10&offset=0&rating=PG-13&lang=en";
+
+    console.log(queryItem)
+    gifGetter(queryItem)
+    console.log(queryURL)
 
 })
 
